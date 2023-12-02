aaab46d023ff86a846f81be22893a76f5693f00f
ElG7 <oyanadergiorgio@gmail.com>
2023-12-02 04:54:10 -0300

    actualizacion mvc

---
+++ PAGINA WEB MVC/Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/TIS/PAGINA%20WEB%20MVC/";
+const HOST = "magallanes.inf.unap.cl";
+const USER = "brojas";
+const PASS = "Gt95x5cDq1";
+const DB = "brojas";
+const CHARSET = "charset=utf8";
+const TITLE = "Gelateria Milano";
+const MONEDA = "CLP";
+const CLIENT_ID = "";
+
+const USER_SMTP = "Tu correo";
+const PASS_SMTP = "contraseña generada para esta aplicacion";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
